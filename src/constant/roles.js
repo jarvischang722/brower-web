b9d3592fa50b041a1b4d4257e2d6854a8e2dfd35
Bin <mongris@gmail.com>
2017-11-16 15:30:43 +1300

    Correct roles value.

--- src/constant/roles.js
+++ src/constant/roles.js
@@ -1,7 +1,7 @@
 const roles = [
   { value: 0, code: 'superadmin' },
-  { value: 2, code: 'agent' },
-  { value: 3, code: 'client' },
+  { value: 1, code: 'agent' },
+  { value: 2, code: 'client' },
 ]
 
 export const updateLocale = () => {
